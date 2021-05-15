7b4c7e8d1f0e261a889a73114c0a9b57997d5fbd
Jonathan Adithya <jo.adithya391@gmail.com>
2021-05-15 11:13:10 +0700

    create the password generator function

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 from tkinter import *
 from tkinter import messagebox
+from random import randint, choice, shuffle
+import pyperclip
+
+BG_COLOR = '#756de7'
+FONT = ('Arial', 14, 'normal')
+FONT_INPUT = ('Arial', 16, 'normal')
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def password_generator():
+    nr_letters= randint(8, 12)
+    nr_symbols = randint(2, 4)
+    nr_numbers = randint(3, 6)
+
+    password = [choice(letters) for _ in range(nr_letters)]
+    password.extend([choice(symbols) for _ in range(nr_symbols)])
+    password.extend([choice(numbers) for _ in range(nr_numbers)])
+
+    shuffle(password)
+    password = "".join(password)
+    is_ok = messagebox.askokcancel(title='Password Generator', message=f'Password: {password}')
+    if is_ok:
+        pass_input.insert(0, password)
+        pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def add_entry():
